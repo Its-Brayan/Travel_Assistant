61e8421d0debd301ee0142f25c093cdf8f5dcb53
Its-Brayan <brayanmwangi732@gmail.com>
2026-06-11 11:46:32 +0300

    fix:Changed currency converter mcp

--- Workflows/graph.py
+++ Workflows/graph.py
@@ -130,7 +130,7 @@ async def run_pipeline(query:str):
      print(f"\n{'='*60}\n")
      print("Starting MCP connection...")
      mcp_env = os.environ.copy()
-     mcp_env["EXCHANGE_RATE_API_KEY"] = mcp_env['EXCHANGE_RATE_API_KEY']
+     mcp_env["EXCHANGE_RATE_API_KEY"] = os.getenv('EXCHANGE_RATE_API_KEY')
      servers ={
          "weather":StdioServerParameters(
          command=sys.executable,
